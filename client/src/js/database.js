e412cd646db5124d86d58102c692e80ac897f332
josh.moran501 <josh.moran501@gmail.com>
2022-09-19 12:32:52 -0400

    minor changes to database.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -17,9 +17,9 @@ export const putDb = async (content) => {
   const jateDB = await openDB("jate", 1);
   const transaction = jateDB.transaction("jate", "readwrite");
   const store = transaction.objectStore("jate");
-  const request = store.put({ jate: content });
+  const request = store.put({ id: 1, value: content });
   const result = await request;
-  console.log("data saved to jate database", result);
+  console.log("data saved to jate database", result.value);
 };
 
 // TODO: Add logic for a method that gets all the content from the database
@@ -28,9 +28,12 @@ export const getDb = async () => {
   const jateDB = await openDB("jate", 1);
   const transaction = jateDB.transaction("jate", "readonly");
   const store = transaction.objectStore("jate");
-  const request = store.getAll();
+  const request = store.get(1);
   const result = await request;
-  console.log(result);
+  result
+    ? console.log("data retrieved from jate database", result.value)
+    : console.log("data not found in jate database");
+  return result?.value;
 };
 
 initdb();
