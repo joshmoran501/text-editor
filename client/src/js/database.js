257e0b31b13f33d702818d3c68b34270f5b1a832
josh.moran501 <josh.moran501@gmail.com>
2022-09-19 12:54:22 -0400

    added scripts to root package.json

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -14,9 +14,10 @@ const initdb = async () =>
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
+  console.log("put to the database");
   const jateDB = await openDB("jate", 1);
-  const transaction = jateDB.transaction("jate", "readwrite");
-  const store = transaction.objectStore("jate");
+  const tx = jateDB.transaction("jate", "readwrite");
+  const store = tx.objectStore("jate");
   const request = store.put({ id: 1, value: content });
   const result = await request;
   console.log("data saved to jate database", result.value);
@@ -26,8 +27,8 @@ export const putDb = async (content) => {
 export const getDb = async () => {
   console.log("get all from database");
   const jateDB = await openDB("jate", 1);
-  const transaction = jateDB.transaction("jate", "readonly");
-  const store = transaction.objectStore("jate");
+  const tx = jateDB.transaction("jate", "readonly");
+  const store = tx.objectStore("jate");
   const request = store.get(1);
   const result = await request;
   result
